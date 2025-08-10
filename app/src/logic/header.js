72b23d017ac7de6309e98c1901ed30906f6ba0b2
ken7python <active.ken.315@gmail.com>
2025-08-10 17:50:18 +0900

    Add header.js to correct header logic

---
+++ app/src/logic/header.js
@@ -0,0 +1,38 @@
+export const header = {
+    getColor: function(menu, trueMenu){
+        if (menu === trueMenu) {
+            return '#FFFFFF';
+        } else {
+            return '#739DDB';
+        }
+    },
+
+    fontsize: '24px',
+    getLinkStyle: function(menu, trueMenu){
+        const weight = menu === trueMenu ? 'bold' : 'normal';
+        return {
+            color: header.getColor(menu,trueMenu),
+            fontWeight: weight,
+            fontSize: header.fontsize
+        }
+    },
+
+    getUnderlineStyle: function(menu, trueMenu) {
+        return {
+            width: '100%',
+            height: '8px',
+            backgroundColor: menu === trueMenu ? '#66BEBC' : '',
+        };
+    },
+
+    getIconStyle: function(trueMenu) {
+        return {
+            // color: header.getColor(trueMenu),
+            width: '30px',
+            height: '30px',
+            display: 'inline-block',
+            position: 'relative',
+            top: '5px',
+        }
+    },
+}
