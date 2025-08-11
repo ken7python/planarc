04ccbc9a8448b2f58070c1138d586a578aeb76c0
ken7python <active.ken.315@gmail.com>
2025-08-11 17:08:39 +0900

    Add CORS setting to test npm run preview

--- server/main.go
+++ server/main.go
@@ -34,7 +34,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:4173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
