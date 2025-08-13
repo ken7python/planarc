652e914ad8d824423eb66a8028b1ae51e7365bff
ken7python <active.ken.315@gmail.com>
2025-08-13 14:35:44 +0900

    Add needly to auth in unfinished page

--- app/src/views/UnfinishedList.vue
+++ app/src/views/UnfinishedList.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+  import {ref} from "vue";
+  import { user } from "@/logic/user.js";
 
+  const username = ref<string | null>(null);
+  async function loadData() {
+    const profile = await user.profile();
+    // console.log(profile);
+    username.value = profile.username;
+  }
+  loadData();
 </script>
 
 <template>
