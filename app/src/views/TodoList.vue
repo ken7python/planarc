ff1f96638f3b55a97f764c244ac34b9c06945815
ken7python <active.ken.315@gmail.com>
2025-08-13 22:26:46 +0900

    Add List Style in TODOLIST Page

--- app/src/views/TodoList.vue
+++ app/src/views/TodoList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import { ref } from 'vue';
-  import { selectStyle } from '@/logic/style/selectStyle.ts';
+  import { selectStyle } from '@/logic/style/selectStyle';
+  import {getColorboxStyle} from "@/logic/style/colorbox";
 
   let subjectName = ref('');
   let todoText = ref('');
@@ -14,7 +15,18 @@
     { name: "社会", value :"shakai" }
   ]
 
-
+  const TODO = ref([
+    { name: '国語', color: '#FF5733' },
+    { name: '数学', color: '#33FF57' },
+    { name: '英語', color: '#3357FF' },
+    { name: '理科', color: '#F1C40F' },
+    { name: '社会', color: '#8E44AD' },
+    { name: '歴史', color: '#FF8C00' },
+    { name: '地理', color: '#00CED1' },
+    { name: '物理', color: '#8A2BE2' },
+    { name: '化学', color: '#FF4500' },
+    { name: '生物', color: '#2E8B57' }
+  ]);
 </script>
 
 <template>
@@ -37,9 +49,25 @@
       <br>
       <button class="btn" style="margin: 0 auto;">追加</button>
     </div>
-    <div id="TODOList">
-      <p>TODOのリストを追加予定</p>
-    </div>
+    <br>
+  </div>
+
+  <div id="List">
+    <p style="color: white;line-height: 0">TODOリストのサンプル(まだ追加できません)</p>
+    <ul class="list-ul">
+      <li class="list-item" v-for="(task, index) in TODO" :key="index">
+        <div>
+          <span :style="getColorboxStyle(task.color)" style="margin-right: 4px;margin-left: 4px;"></span>
+          <span>{{ task.name }}</span>
+        </div>
+        <div class="right">
+<!--          <button class="squareBtn btnTrash" style="margin-right: 4px;margin-left: 4px;"></button>-->
+          <input type="checkbox" class="squareBtn btnCheck" style="margin-right: 4px;margin-left: 4px;" />
+          <button class="squareBtn btnUnfinished" style="margin-right: 4px;margin-left: 4px;"></button>
+
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
