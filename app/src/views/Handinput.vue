0f95b3f71567c68ea25add1fca83678c147ec11d
ken7python <active.ken.315@gmail.com>
2025-08-16 04:48:39 +0900

    Complete handinput Basic UI

--- app/src/views/Handinput.vue
+++ app/src/views/Handinput.vue
@@ -1,16 +1,56 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+  import { selectStyle } from '@/logic/style/selectStyle';
+  import writeIcon from '@/assets/icons/write.svg';
+  let subjectName = ref<string>('');
 
+  const subjects = [
+    { name: "国語", value :"kokugo" },
+    { name: "数学", value :"sugaku" },
+    { name: "英語", value :"eigo" },
+    { name: "理科", value :"rika" },
+    { name: "社会", value :"shakai" }
+  ]
 </script>
 
 <template>
   <div id="headerinput">
-    <input type="text" placeholder="科目名を入力" v-model="subjectName" />
-    <br>
+    <select class="selectbox" :style="selectStyle.getSelectStyle(subjectName)" v-model="subjectName">
+      <option value="">科目を選択</option>
+      <option v-for="subject in subjects" :key="subject.value" :value="subject.value">
+        {{ subject.name }}
+      </option>
+    </select>
+    <div id="inputbox">
+      <div>
+        <h2>開始時間</h2>
+        <input type="time" id="startTime" name="startTime" value="--:--">
+      </div>
+      <div>
+        <h2>終了時間</h2>
+        <input type="time" id="startTime" name="startTime" value="--:--">
+      </div>
+    </div>
+    <div id="res">
+      <button class="btn"  style="margin: 0 auto;"><writeIcon></writeIcon>記録</button>
+    </div>
   </div>
 </template>
 
 <style scoped>
   #headerinput {
     text-align: center;
+  }
+
+  #inputbox {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 10px 0;
+    margin-bottom: 80px;
+  }
+
+  .btn{
+    width: 200px;
   }
 </style>
