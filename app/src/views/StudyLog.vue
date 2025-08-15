e676abaaf124d9e696c2b82d0d39d85507f3fa3a
ken7python <active.ken.315@gmail.com>
2025-08-16 04:13:02 +0900

    Add Menu in studyLog

--- app/src/views/StudyLog.vue
+++ app/src/views/StudyLog.vue
@@ -1,13 +1,61 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+  import _default from "chart.js/dist/plugins/plugin.tooltip";
+  const strongGray = "#737373"
+  const lightGray = "#F2F2F2"
 
+  const stopwatch = "stopwatch";
+  const handinput = "handinput";
+
+  let selectedMenu = ref(stopwatch);
+
+  function select(menu: string) {
+    selectedMenu.value = menu;
+  }
+
+  function selected(menu: string): boolean {
+    return selectedMenu.value === menu;
+  }
+
+ function getMenuStyle(menu: string): object {
+    return {
+      backgroundColor: selected(menu) ? strongGray : lightGray,
+      color: selected(menu) ? "#FFFFFF" : "#000000",
+      cursor: "pointer",
+      padding: "10px",
+      borderRadius: "5px"
+    };
+  }
 </script>
 
 <template>
   <div>
-    <p>勉強記録画面を作成予定</p>
+    <div id="menu">
+      <div id="stopwatch" :style="getMenuStyle(stopwatch)" @click="select(stopwatch)">
+        <a>ストップウォッチ</a>
+      </div>
+      <div id="handinput" :style="getMenuStyle(handinput)" @click="select(handinput)">
+        <a>手動入力</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+  #menu {
+    width: 100%;
+    height: 60px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    background-color: #f0f0f0;
+    border-bottom: 1px solid #ccc;
+    text-align: center;
+  }
+  #stopwatch, #handinput{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    color: #333;
+  }
 </style>
