f0bfcb264a77ec727ebc2e1c9f67a1d5c1ca20e3
ken7python <active.ken.315@gmail.com>
2025-08-16 17:48:54 +0900

    Add New HOME UI

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -4,36 +4,121 @@
 
   import Footer from "@/components/Footer.vue";
   import Header from "@/components/Header.vue";
-  import HeaderHome from "@/components/HeaderHOME.vue";
-  import { header } from "@/logic/header.js";
 
-  import StudyTime from "./StudyTime.vue";
-  import AddSubject from "./AddSubject.vue";
+  import { selectStyle } from '@/logic/style/selectStyle';
+  import { subjectModule } from '@/logic/subject';
 
-  const log = header.log;
-  const subject = header.subject;
-  const activeMenu = ref< log | subject >(log);
+  import mode1 from '@/assets/icons/1.svg';
+  import mode2 from '@/assets/icons/2.svg';
+  import mode3 from '@/assets/icons/3.svg';
+  import mode4 from '@/assets/icons/4.svg';
 
+  import { mic } from '@/logic/mic';
+  import MicIcon from '@/assets/icons/mic.svg';
 
+  let subjectName = ref<string>('');
+
+  let enjoyment = ref<string>('');
+
+  let subjects = ref<any[]>([]);
   const username = ref<string | null>(null);
   async function loadData() {
     const profile = await user.profile();
     // console.log(profile);
     username.value = profile.username;
+
+    const subject_list = await subjectModule.getList();
+    console.log(subject_list);
+    subjects.value = subject_list;
   }
   loadData();
+
+  async function micbtn() {
+    // alert('マイクボタンが押されました。');
+    if (!mic.shouldRestart) {
+      console.log("start");
+      mic.result = '';
+      mic.micON.value = true;
+      await mic.start();
+    } else {
+      console.log("stop");
+      mic.micON.value = false;
+      await mic.stop();
+    }
+  }
+
+  mic.rec.onresult = (e) => {
+    let interim = '';
+    for (let i = e.resultIndex; i < e.results.length; i++) {
+      const transcript = e.results[i][0].transcript;
+      if (e.results[i].isFinal) {
+        mic.result += transcript;
+        mic.tmp = '';
+        enjoyment.value = mic.result;
+      } else {
+        interim += transcript;
+        mic.tmp = interim;
+        enjoyment.value = mic.result + mic.tmp;
+      }
+    }
+  };
 </script>
 
 <template>
-  <div>
+  <div id="tmp">
     <header>
       <Header />
 <!--      <HeaderHome v-model:menu="activeMenu" />-->
     </header>
     <div id="main">
-<!--    {{ activeMenu }}-->
-<!--      <study-time v-if="activeMenu === log" />-->
-<!--      <add-subject v-if="activeMenu === subject" />-->
+      <h3>今日の楽しみ</h3>
+      <div class="micdiv">
+        <input type="text" placeholder="今日の楽しみを入力してください" v-model="enjoyment" style="height: 30px;" />
+        <mic-icon :style="mic.micStyle()" @click="micbtn"></mic-icon>
+      </div>
+
+      <div id="feeling">
+        <h3>今日の気分</h3>
+        <div id="feeling-icons">
+          <mode4></mode4>
+          <mode3></mode3>
+          <mode2></mode2>
+          <mode1></mode1>
+        </div>
+      </div>
+
+      <div id="loginbonus">
+        <h3>〇日連続ログイン</h3>
+        <h3>累計〇日使用</h3>
+      </div>
+
+      <div id="studyplan">
+        <h3>学習予定一覧</h3>
+        <ul>
+          <li>科目1</li>
+          <li>科目2</li>
+          <li>科目3</li>
+        </ul>
+      </div>
+
+      <div id="progress">
+        <h3>進捗バー</h3>
+        <progress value="50" max="100"></progress>
+        <p>50/100</p>
+      </div>
+
+      <div id="studyTime">
+        <h3>今日の勉強時間〇〇時間</h3>
+
+        <h3>今日の勉強時間（科目別）：〇〇時間</h3>
+        <select class="selectbox" :style="selectStyle.getSelectStyle(subjectName)" v-model="subjectName">
+          <option value="">科目を選択</option>
+          <option v-for="subject in subjects" :key="subject.value" :value="subject.ID">
+            {{ subject.Name }}
+          </option>
+        </select>
+      </div>
+
     </div>
     <footer>
       <Footer />
@@ -42,5 +127,28 @@
 </template>
 
 <style scoped>
+h3 {
+  line-height: 0.75;
+}
+ progress {
+    width: 100%;
+    height: 20px;
+    margin: 0;
+ }
 
+ #main {
+   overflow-y: scroll;
+ }
+ #tmp {
+   height: calc(100dvh - 80px);
+   display: grid;
+   grid-template-rows: auto 1fr auto; /* ヘッダー=auto / 中央=残り / フッター=auto */
+ }
+
+ #feeling-icons {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 30px;
+ }
 </style>
