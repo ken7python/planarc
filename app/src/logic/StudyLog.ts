9772edc8d0f51e05166d568617e40ae93eadf5e5
ken7python <active.ken.315@gmail.com>
2025-08-17 11:50:25 +0900

    Create StopWatch UI and frontend logics

--- app/src/logic/StudyLog.ts
+++ app/src/logic/StudyLog.ts
@@ -1,17 +1,98 @@
 import { ref } from 'vue';
 import { CONST } from "@/logic/const";
 
-const stopwatch = {
-    startTime: ref(null),
-    endTime: ref(null),
-    start: function() {
-        this.startTime.value = new Date();
-        this.endTime.value = null;
+export const stopwatch = {
+    startDate: ref(null),
+    endDate: ref(null),
+
+    DateDiff: {
+        hours: ref(null),
+        minutes: ref(null),
     },
-    end: function() {
-        if (this.startTime.value === null) {
+
+    sHours: ref(null),
+    sMinutes: ref(null),
+    eHours: ref(null),
+    eMinutes: ref(null),
+    dHours: ref(null),
+    dMinutes: ref(null),
+
+    init: function() {
+        this.startDate.value = null;
+        this.endDate.value = null;
+        this.DateDiff.value = null;
+
+        this.sHours.value = null;
+        this.sMinutes.value = null;
+        this.eHours.value = null;
+        this.eMinutes.value = null;
+        this.dHours.value = null;
+        this.dMinutes.value = null;
+
+        this.update();
+    },
+
+    update: function() {
+        const sd = this.startDate.value;
+        const ed = this.endDate.value;
+
+        let dMinutes = this.dMinutes
+        let dHours = this.dHours;
+
+        let eMinutes = this.eMinutes;
+        let eHours = this.eHours;
+        let sMinutes = this.sMinutes;
+        let sHours = this.sHours;
+        if (sd) {
+            sMinutes.value = this.startDate.value.getMinutes();
+            sHours.value = this.startDate.value.getHours();
+        }else {
+            sMinutes.value = "--";
+            sHours.value = "--";
+        }
+        if (ed) {
+            eMinutes.value = this.endDate.value.getMinutes();
+            eHours.value = this.endDate.value.getHours();
+        }else {
+            eMinutes.value = "--";
+            eHours.value = "--";
+        }
+        if (sd) {
+            // dMinutes.value = null;
+            // dHours.value = null;
+            let diff;
+            const tStart = sMinutes.value + sHours.value * 60;
+            if (ed) {
+                const tStop = eMinutes.value + eHours.value * 60;
+                diff = tStop - tStart;
+            }else {
+                const now = new Date();
+                const nMinute = now.getMinutes();
+                const nHour = now.getHours();
+                const tNow = nMinute + nHour * 60;
+                diff = tNow - tStart;
+            }
+            dMinutes.value = diff % 60;
+            dHours.value = Math.floor(diff / 60);
+        }
+
+
+        console.log(this);
+    },
+
+    start: function() {
+        this.startDate.value = new Date();
+        console.log(this.startDate.value);
+        this.endDate.value = null;
+
+        this.update();
+    },
+    stop: function() {
+        if (this.startDate.value === null) {
             alert("ストップウォッチは開始していません")
         }
-        this.endTime.value = new Date();
-    }
+        this.endDate.value = new Date();
+
+        this.update();
+    },
 }
--- app/src/views/Stopwatch.vue
+++ app/src/views/Stopwatch.vue
@@ -2,10 +2,12 @@
   import { ref } from 'vue';
   import { selectStyle } from '@/logic/style/selectStyle';
   import { subjectModule } from '@/logic/subject';
+  import { stopwatch } from '@/logic/StudyLog';
 
   import startIcon from '@/assets/icons/start.svg';
   import stopIcon from '@/assets/icons/stop.svg';
   import writeIcon from '@/assets/icons/write.svg';
+
   let subjectName = ref<string>('');
 
   let subjects = ref<any[]>([]);
@@ -13,9 +15,15 @@
     const subject_list = await subjectModule.getList();
     console.log(subject_list);
     subjects.value = subject_list;
+
+    stopwatch.init()
   }
 
   loadData();
+
+  setInterval(function() {
+    stopwatch.update();
+  }, 1000);
 </script>
 
 <template>
@@ -26,16 +34,24 @@
         {{ subject.Name }}
       </option>
     </select>
+
     <div id="start">
-      <button class="btn"><startIcon></startIcon>開始</button>
-      <span class="time">--:--</span>
+      <button class="btn" @click="stopwatch.start()"><startIcon></startIcon>開始</button>
+      <span class="time">
+        <span v-if="stopwatch.sHours<10">0</span>{{ stopwatch.sHours }}:<span v-if="stopwatch.sMinutes<10">0</span>{{ stopwatch.sMinutes }}
+      </span>
     </div>
     <div id="end">
-      <button class="btn"><stopIcon></stopIcon>終了</button>
-      <span class="time">--:--</span>
+      <button class="btn" @click="stopwatch.stop()"><stopIcon></stopIcon>終了</button>
+      <span class="time">
+        <span v-if="stopwatch.eHours<10">0</span>{{ stopwatch.eHours }}:<span v-if="stopwatch.eMinutes<10">0</span>{{ stopwatch.eMinutes }}
+      </span>
     </div>
     <div id="res">
-      <span class="time">--:--</span>
+      <span class="time">
+        <span v-if="stopwatch.dHours.value != null">{{ stopwatch.dHours }}時間</span>
+        <span v-if="stopwatch.dMinutes.value != null">{{ stopwatch.dMinutes.value }}分</span>
+      </span>
       <button class="btn"  style="margin: 0 auto;"><writeIcon></writeIcon>記録</button>
     </div>
   </div>
@@ -56,7 +72,7 @@
     width: 200px;
   }
   .time {
-    font-size: 64px;
+    font-size: 42px;
     margin-left: 10px;
   }
 </style>
