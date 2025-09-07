1bf05e99b85c753dbf5952fb14149bbb6aa776a8
ken7python <active.ken.315@gmail.com>
2025-09-07 16:56:53 +0900

    Add function to comment by geminiAPI

--- server/gemini.go
+++ server/gemini.go
@@ -3,13 +3,113 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"os"
-
+	"github.com/gin-gonic/gin"
 	"google.golang.org/genai"
+	"log"
+	"net/http"
+	"os"
 )
 
-func askGemini() {
+func getPrompt(uuid string, date string, name string, note string) string {
+	// Status取得
+	status := retGetStatus(uuid, date)
+	//strStatus := fmt.Sprintf("Mood: %v, Enjoyment: %v", status.Mood, status.Enjoyment)
+	//fmt.Println(strStatus)
+
+	subjectID := retGetSubjectByUserID(uuid)
+
+	todos := retGetTODOByUserID(uuid, date)
+	//fmt.Println("ToDos:", todos)
+
+	strToDos := ""
+	for _, v := range todos {
+		todo := fmt.Sprintf("{Title: %v, Checked: %v Status: %v}\n", v.Title, v.Checked, v.Status)
+		strToDos += todo
+	}
+
+	//fmt.Println(strToDos)
+
+	logs := retGetLogByUserID(uuid, date)
+	//fmt.Println("StudyLogs:", logs)
+	sum := 0
+	for _, slog := range logs {
+		sum += slog.StudyTime
+	}
+
+	var logstr string = ""
+	for _, slog := range logs {
+		str := fmt.Sprintf("{ SubjectID: %v,StartHour: %v,StartMinute: %v,EndHour: %v,EndMinute %v,StudyTime: %v}\n", slog.SubjectID, slog.StartHours, slog.StartMinutes, slog.EndHours, slog.EndMinutes, slog.StudyTime)
+		logstr += str
+	}
+	//fmt.Println(logstr)
+
+	unfinished := retGetUnfinishedByUserID(uuid)
+	//fmt.Println("Unfinished:", unfinished)
+
+	strUnfinished := ""
+	for _, list := range unfinished {
+		strUnfinished += fmt.Sprintf("{Title: %v, Status: %v, Date: %v,SubjectID: %v}\n", list.Title, list.Status, list.Date, list.SubjectID)
+	}
+
+	mood := status.Mood
+	enjoyment := status.Enjoyment
+	studyTime := fmt.Sprintf("%d時間%d分", sum/60, sum%60)
+	todolist := strToDos
+	strTrack := logstr
+
+	prompt := fmt.Sprintf(`あなたは、私の学校の先生です。
+私の呼び名は%sです。
+私が勉強をがんばれるような声かけをするのが得意です。
+次のようなステップで300字程度コメントしてください。
+今日は%sです
+なお、返事は来ないものとしてマークダウンではなく平文で作成してください。
+
+1.今日の取り組み成果に対する評価
+2.明日以降の提案
+3.今日の気分や振り返りコメントに関連したこと
+
+## 添付情報
+### 今日の気分（4段階）
+%d
+### 今日の楽しみ
+%s
+### 勉強時間
+%s
+### 全科目ID一覧(ToDoやStudyLog。未完了リストのSubjectIDに対応)
+%s
+### ToDoの達成状況
+%s
+### TimeTracking
+%s
+### 未完了リスト
+%s
+### 今日の振り返り
+%s
+`, name, date, mood, enjoyment, studyTime, subjectID, todolist, strTrack, strUnfinished, note)
+	fmt.Println(prompt)
+	return prompt
+}
+
+func reqComment(c *gin.Context) {
+	uuid := GetProfile(c).UUID
+
+	type RequestBody struct {
+		Date string `json:"Date"`
+		Note string `json:"Note"`
+	}
+	var req RequestBody
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	var name string = "あなた"
+	if GetProfile(c) != nil {
+		name = GetProfile(c).Username
+	}
+
 	ctx := context.Background()
 
 	apiKey := os.Getenv("GEMINI_API_KEY")
@@ -21,25 +121,35 @@ func askGemini() {
 		Backend: genai.BackendGeminiAPI, // AI StudioのGemini APIを使う
 		APIKey:  apiKey,
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	//defer client.Close()
 
-	contents := []*genai.Content{{
-		Parts: []*genai.Part{{Text: "あなたは私の学校の先生です。私が勉強をがんばれるような声をかけるのが得意です。今日の勉強時間は2時間30分です。完了したのは英語の長文問題集とインターンシップの作文です。未完了は数学IAのワークと英単語です。振り返り文には課題がたまっていたけれど、少し進められてよかったと書いていました。手帳のコメントとしてメッセージをお願いします"}},
-	}}
-
-	resp, err := client.Models.GenerateContent(ctx, "gemini-2.5-flash", contents, nil)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("failed to create client: %v", err)
+		c.JSON(500, gin.H{"error": "AIクライアントの作成に失敗しました"})
+		return
 	}
 
-	for _, cand := range resp.Candidates {
-		for _, p := range cand.Content.Parts {
-			if p.Text != "" {
-				fmt.Println(p.Text)
-			}
+	modelName := "gemini-2.5-flash"
+
+	prompt := getPrompt(uuid, req.Date, name, req.Note)
+
+	contents := genai.Text(prompt)
+
+	response := ""
+
+	c.Writer.Header().Set("Content-Type", "text/event-stream")
+	flusher, _ := c.Writer.(http.Flusher)
+
+	for resp, err := range client.Models.GenerateContentStream(ctx, modelName, contents, nil) {
+		if err != nil {
+			fmt.Printf("failed to generate content: %w", err)
+			c.JSON(500, gin.H{"error": "コンテンツの生成に失敗しました"})
+			return
 		}
+
+		chunk := resp.Text()
+		//fmt.Println(chunk)
+		response += chunk
+		fmt.Fprintf(c.Writer, "%s\n\n", chunk)
+		flusher.Flush()
 	}
 }
