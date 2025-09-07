bc1d3311b5e0d0e5210283247a5bbf3bbfa01124
ken7python <active.ken.315@gmail.com>
2025-09-07 10:23:55 +0900

    Create Table

--- server/status.go
+++ server/status.go
@@ -1,1 +1,9 @@
 package main
+
+type Status struct {
+	ID        uint   `gorm:"primaryKey"`
+	UUID      string `gorm:"not null"`
+	Date      string `gorm:"not null"`
+	Enjoyment string `gorm:"not null"`
+	mood      uint   `gorm:"not null"`
+}
